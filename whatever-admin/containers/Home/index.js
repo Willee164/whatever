3218762cc3d6ad325c6330241214a03dd52fe314
Willee164 <87996780+Willee164@users.noreply.github.com>
2021-08-09 07:15:42 +0700

    Add files via upload

---
+++ whatever-admin/containers/Home/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import { Container, Row, Col } from 'react-bootstrap';
+import './style.css';
+import { NavLink } from 'react-router-dom';
+
+/**
+* @author
+* @function Home
+**/
+
+const Home = (props) => {
+
+  
+
+  return(
+    <Layout sidebar>
+
+
+
+      
+        {/* <Jumbotron style={{margin: '5rem', background: '#FFF'}} className="text-center">
+            <h1>Welcome to Admin Dashboard</h1>
+            <p>hahahahahahahaha</p>
+        </Jumbotron> */}
+    </Layout>
+   )
+
+ }
+
+export default Home
